7469d2e4d5800844c4c7df78346c03549f92b61f
pikesley <github@orgraphone.org>
2014-09-10 10:53:30 +0100

    Move cron settings on attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,16 @@ default['envbuilder']['base_dir'] = '/home/env'
 default['envbuilder']['base_dbi'] = 'production'
 default['envbuilder']['owner'] = 'env'
 default['envbuilder']['group'] = 'env'
+
+default['chef_client']['splay'] = 250
+default['chef_client']['cron']['use_cron_d'] = true
+default['chef_client']['cron']['hour'] = '*'
+default['chef_client']['cron']['minute'] = '*/5'
+default['chef_client']['cron']['log_file'] = '/var/log/chef/cron.log'
+#      chef_client:
+#        splay: 250
+#        cron:
+#          use_cron_d: true
+#          hour:       '*'
+##          minute:     '*/5'
+  #        log_file:   /var/log/chef/cron.log
