317fb9dc69c423e3523cb42ca6073f87622258f6
pikesley <github@orgraphone.org>
2014-09-29 11:16:20 +0100

    Test this stuff

--- test/integration/default/serverspec/localhost/envbuilder_spec.rb
+++ test/integration/default/serverspec/localhost/envbuilder_spec.rb
@@ -2,4 +2,7 @@ require 'spec_helper'
 
 describe file('/home/env/env') do
   it { should be_file }
+  its(:content) { should match /DOGE: coin/ }
+  its(:content) { should match /CORN: snake/ }
+  its(:content) { should match /BAG: prod/ }
 end
