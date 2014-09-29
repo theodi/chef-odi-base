cb489b0fd7e459159b8c6b07303caac79ab7baca
pikesley <github@orgraphone.org>
2014-09-29 11:14:23 +0100

    Cruft

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,10 +7,3 @@ default['chef_client']['cron']['use_cron_d'] = true
 default['chef_client']['cron']['hour'] = '*'
 default['chef_client']['cron']['minute'] = '*/5'
 default['chef_client']['cron']['log_file'] = '/var/log/chef/cron.log'
-#      chef_client:
-#        splay: 250
-#        cron:
-#          use_cron_d: true
-#          hour:       '*'
-##          minute:     '*/5'
-  #        log_file:   /var/log/chef/cron.log
